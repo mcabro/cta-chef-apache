8822131425a9dc45a1e057b351e736117e7069a7
McAbro <mcabro@gmail.com>
2019-06-25 17:30:45 +0200

    Añadido cookbook myhaproxy. Es un wrapper de una versión vieja del haproxy del supermarket.

---
+++ myhaproxy/attributes/default.rb
@@ -0,0 +1,16 @@
+
+node.default['haproxy']['members'] = [
+    {
+        'hostname' => 'web1',
+        'ipaddress' => '18.208.193.188',
+        'port' => 80,
+        'ssl_port' => 80,
+    },
+    {
+        'hostname' => 'web2w',
+        'ipaddress' => '54.234.3.30',
+        'port' => 80,
+        'ssl_port' => 80,
+    },    
+]
+
---
+++ myhaproxy/recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: myhaproxy
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+include_recipe 'haproxy::manual'
